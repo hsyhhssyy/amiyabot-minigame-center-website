f8b988c105fa8b228a8dceb2bee2fba27987a3d8
hsyhhssyy <hsyhhssyy@gmail.com>
2024-05-16 05:24:44 +0000

    store

--- public/config.js
+++ public/config.js
@@ -1,4 +1,5 @@
 window._env_ = {
     // VUE_APP_API_URL: "https://amiya-bot-service.hsyhhssyy.net"
-    VUE_APP_API_URL: "https://game-api.anonymous-test.top"
+    // VUE_APP_API_URL: "https://game-api.anonymous-test.top"
+    VUE_APP_API_URL: "https://game-api.hsyhhssyy.net"
 };
