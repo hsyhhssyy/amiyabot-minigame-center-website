e8ed4a618d68f5365187c285d3ec9113a9a9bbf0
hsyhhssyy <hsyhhssyy@gmail.com>
2024-05-29 05:16:49 +0000

    route调整完毕

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,45 @@
 <template>
-    <DesktopApp />
+    <component :is="layoutComponent" :key="key"></component>
 </template>
 
 <script setup lang="ts">
+import { ref, computed, watch, onMounted, onUnmounted } from 'vue';
+import { useRoute,useRouter } from 'vue-router';
 import DesktopApp from "@/desktop/DesktopApp.vue"
+import MobileApp from "@/mobile/MobileApp.vue"
+
+const route = useRoute();
+const router = useRouter();
+const key = ref(0);
+const isMobile = ref(window.innerWidth < 768);
+
+const layoutComponent = computed(() => {
+if (route.path.startsWith('/m/')) {
+    return MobileApp;
+  } else {
+    return DesktopApp;
+  }
+})
+
+const handleResize = () => {
+  isMobile.value = window.innerWidth < 768;
+  if (isMobile.value && !route.path.startsWith('/m/')) {
+    router.replace(`/m${route.fullPath}`);
+  }
+};
+
+onMounted(() => {
+  window.addEventListener('resize', handleResize);
+  handleResize(); // 初始化检查
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize);
+});
+
+watch(route, () => {
+  key.value++;
+});
 
 </script>
 
---
+++ src/mobile/MobileApp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <router-view />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.container {
+    width: 100%;
+    height: 100%;
+    font-size: 14px;
+    overflow: hidden;
+}
+</style>
