44a649ec254bffa8a5164b3e305b78f2b30d89d2
hsyhhssyy <hsyhhssyy@gmail.com>
2024-06-02 12:00:11 +0000

    加入按钮防抖

--- src/universal/components/IconButton.vue
+++ src/universal/components/IconButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <n-button :type="props.type || 'default'" :size="props.size || 'medium'" :text="text">
+    <n-button :type="props.type || 'default'" :size="props.size || 'medium'" :text="text" @click="handleClick">
         <template #icon>
             <icon :icon="props.icon" />
         </template>
@@ -17,6 +17,28 @@ const props = defineProps<{
     size?: Size
     text?: boolean
 }>()
+
+const emit = defineEmits(['click'])
+
+function debounce<T extends (...args: any[]) => any>(func: T, wait: number): T {
+  let timeout: NodeJS.Timeout | undefined
+  return function (this: ThisParameterType<T>, ...args: Parameters<T>): void {
+    const context = this
+    clearTimeout(timeout)
+    timeout = setTimeout(() => func.apply(context, args), wait)
+  } as T
+}
+
+const debouncedClick = debounce(() => {
+  console.log('debounced click')
+  emit('click')
+}, 300)
+
+function handleClick() {
+  debouncedClick()
+}
+
+
 </script>
 
 <style lang="scss" scoped></style>
